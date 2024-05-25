b706e12e8fcf4cc7e879d7bb766a37156607579c
Keerthika Chokkalingam <142724355+KeerthikaChokkalingam@users.noreply.github.com>
2024-05-25 19:26:34 +0530

    commit with inital-setup

---
+++ zStore/zStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zStore
+//
+//  Created by Keerthika on 25/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blue
+    }
+
+
+}
+
